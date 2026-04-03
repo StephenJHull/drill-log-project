769640a99177a72193e1443eecc5e6781b3faced
Stephen Hull <stephen@stephenghull.com>
2026-04-03 15:29:28 -0400

    Add EF core code

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 
